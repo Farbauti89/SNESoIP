154ede9407029f6e4e3dee83189fc247aca729e3
Michael Fitzmayer <mail@michael-fitzmayer.de>
2014-01-05 16:37:30 +0100

    Updated protocol specification.

--- server/protocol.h
+++ server/protocol.h
@@ -12,10 +12,35 @@
 
 
 /* +-------------+-------------+--------+----------------+
- * | Function ID | Data length |  Data  | CRC16 checksum |
- * +-------------+-------------+--------+----------------+
- * | 1 Byte      | 1 Byte      | N byte | 2 Byte         |
- * +-------------+-------------+--------+----------------+ */
+	 | Function ID | Data length |  Data  | CRC16 checksum |
+	 +-------------+-------------+--------+----------------+
+	 | 1 Byte      | 1 Byte      | N byte | 2 Byte         |
+	 +-------------+-------------+--------+----------------+
+
+
+	 A simple login sequence could be implemented as follows:
+
+	 At startup the SNESoIP sends following package to the server.
+	 +-------+----------+-----------+----------+
+	 | Login | LoginLen | Client ID | Checksum |
+   +-------+----------+-----------+----------+
+
+	 On success (e.g. server is up), the server sends following reply:
+	 +-------+----------+-----------+----------+
+	 | Hello | HelloLen | Client ID | Checksum |
+	 +-------+----------+-----------+----------+
+
+	 If received successfully, the SNESoIP goes on by requesting various
+	 configuration parameters (e.g. IP address of the opponent etc.):
+
+	 +-----------+--------------+-----------+----------+
+	 | RequestIP | RequestIPLen | Client ID | Checksum |
+	 +-----------+--------------+-----------+----------+
+
+	 Which would lead to an appropriate reply:
+	 +----+-------+-----+-----+-----+----+----------+
+	 | IP | IPLen | 109 | 193 | 192 | 40 | Checksum |
+	 +----+-------+-----+-----+-----+----+----------+ */
 
 
 // Function IDs.
@@ -26,9 +51,9 @@
 
 
 // Data lengths.
-#define LoginLen       0
-#define HelloLen       0
-#define RequestIPLen   0
+#define LoginLen       1
+#define HelloLen       1
+#define RequestIPLen   1
 #define IPLen          4
 
 
