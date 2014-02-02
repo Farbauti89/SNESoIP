91ef695901036e0d6e9d8a5422b4dc68d34de4ec
Jan Schmitz <pw_returner@web.de>
2014-02-02 22:01:35 +0100

    Edited Models

--- server/web2.0/models/profile.php
+++ server/web2.0/models/profile.php
@@ -8,11 +8,104 @@
  * Author: Jan Schmitz
  */
 
+namespace models;
+
 /**
  * Description of profile
  *
  * @author jan
  */
 class profile {
-    //put your code here
+    
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     * @var int 
+     */
+    private $id;
+    
+    /**
+     * @Column(type="string", length=255)
+     * @var string
+     */
+    private $firstname;
+    
+    /**
+     * @Column(type="string", length=255)
+     * @var string
+     */
+    private $lastname;
+    
+    /**
+     * @Column(type="blob")
+     * @var blob
+     */
+    private $avatar;
+    
+    /**
+     * @Column(type="string", length=255)
+     * @var string
+     */
+    private $region;
+    
+    /**
+     * @Column(type="string", length=255)
+     * @var string
+     */
+    private $email;
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getFirstname() {
+        return $this->firstname;
+    }
+
+    public function getLastname() {
+        return $this->lastname;
+    }
+
+    public function getAvatar() {
+        return $this->avatar;
+    }
+
+    public function getRegion() {
+        return $this->region;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setFirstname($firstname) {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    public function setLastname($lastname) {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    public function setAvatar(blob $avatar) {
+        $this->avatar = $avatar;
+        return $this;
+    }
+
+    public function setRegion($region) {
+        $this->region = $region;
+        return $this;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+        return $this;
+    }
+
+
 }
--- server/web2.0/models/user.php
+++ server/web2.0/models/user.php
@@ -8,10 +8,12 @@
  * Author: Jan Schmitz
  */
 
+namespace models;
+
 /**
  * @Entity @Table(name="user")
  **/
-class User {
+class user {
     
     /**
      * @Id @Column(type="integer") @GeneratedValue
@@ -74,9 +76,125 @@ class User {
     private $destUser;
     
     /**
+     * @OneToOne(targetEntity="Profile")
+     * @JoinColumn(name="id", referencedColumnName="user_id") 
+     */
+    private $profile;
+    
+    /**
      * @Column(type="string", length=64)
      * @var string
      */
     private $key;
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getCreateTime() {
+        return $this->createTime;
+    }
+
+    public function getIsAdmin() {
+        return $this->isAdmin;
+    }
+
+    public function getCurrentIp() {
+        return $this->currentIp;
+    }
+
+    public function getPort() {
+        return $this->port;
+    }
+
+    public function getOnline() {
+        return $this->online;
+    }
+
+    public function getAuthTime() {
+        return $this->authTime;
+    }
+
+    public function getDestUser() {
+        return $this->destUser;
+    }
+
+    public function getKey() {
+        return $this->key;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+        return $this;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function setCreateTime(DateTime $createTime) {
+        $this->createTime = $createTime;
+        return $this;
+    }
+
+    public function setIsAdmin($isAdmin) {
+        $this->isAdmin = $isAdmin;
+        return $this;
+    }
+
+    public function setCurrentIp($currentIp) {
+        $this->currentIp = $currentIp;
+        return $this;
+    }
+
+    public function setPort($port) {
+        $this->port = $port;
+        return $this;
+    }
+
+    public function setOnline($online) {
+        $this->online = $online;
+        return $this;
+    }
+
+    public function setAuthTime(DateTime $authTime) {
+        $this->authTime = $authTime;
+        return $this;
+    }
+
+    public function setDestUser($destUser) {
+        $this->destUser = $destUser;
+        return $this;
+    }
+
+    public function setKey($key) {
+        $this->key = $key;
+        return $this;
+    }
+
+    public function getProfile() {
+        return $this->profile;
+    }
+
+    public function setProfile($profile) {
+        $this->profile = $profile;
+        return $this;
+    }
+
+
             
 }
